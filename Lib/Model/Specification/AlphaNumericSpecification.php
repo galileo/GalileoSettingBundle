b4f10b1c24004225dbdb1e25b0069ae3284825f8
Kamil Ronewicz <galileox86@gmail.com>
2017-03-13 22:44:13 +0100

    Add Alphanumeric specification

---
+++ Lib/Model/Specification/AlphaNumericSpecification.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Galileo\SettingBundle\Lib\Model\Specification;
+
+use Galileo\SettingBundle\Lib\Model\Exceptions\GuardException;
+
+class AlphaNumericSpecification implements Specification
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function guard($value)
+    {
+        if (!$this->satisfiedBy($value)) {
+            throw new GuardException('Only alphanumeric values are accepted');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function satisfiedBy($value)
+    {
+        return !is_object($value) && preg_match('#\w+#', $value);
+    }
+}
